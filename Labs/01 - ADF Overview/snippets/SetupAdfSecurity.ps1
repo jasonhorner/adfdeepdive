b796f4e9bf36a8ee82b002047f084e8a2e46575b
Jason Horner <jasonh@attunix.com>
2019-02-17 12:50:49 +0100

    Update SetupAdfSecurity.ps1

--- Labs/01 - ADF Overview/snippets/SetupAdfSecurity.ps1
+++ Labs/01 - ADF Overview/snippets/SetupAdfSecurity.ps1
@@ -3,16 +3,11 @@ $resourceGroupName = "sqbitsadf01"
 $dataFactoryName = "adfdp01"
 $groupName = "ADF_Access"
 
-
 $myRg = Get-AzResourceGroup $resourceGroupName
-
 
 $myDf = Set-AzDataFactoryV2 -ResourceGroupName $myRg.ResourceGroupName -Location $myRg.Location -Name $dataFactoryName
 
-
 $mySp = Get-AzADServicePrincipal -ObjectId $($myDf.Identity.PrincipalId)
-
-
 
 $group = New-AzADGroup -DisplayName $groupName -MailNickname $groupName
 Add-AzureAdGroupMember -ObjectId $Group.Id -RefObjectId $mysp.Id
