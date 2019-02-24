b127945b434b62fe372fed3224dfd87e5b44ef83
Jason Horner <jasonh@attunix.com>
2019-02-24 18:23:41 +0100

    Create StagingSecurity.sql

---
+++ Labs/01 - ADF Overview/StagingSecurity.sql
@@ -0,0 +1,10 @@
+CREATE USER [ADF_Access] FROM EXTERNAL PROVIDER; 
+
+CREATE USER AnalysisServiceUser FOR LOGIN AnalysisServiceUser;
+CREATE USER DataLoadUser FOR LOGIN DataLoadUser;
+CREATE USER DeploymentUser FOR LOGIN DeploymentUser;
+
+GRANT CONTROL ON DATABASE::[Staging] to [DeploymentUser];
+
+EXEC sp_addrolemember 'Reader', 'AnalysisServiceUser';
+EXEC sp_addrolemember 'Writer', 'ADF_ACCESS';
