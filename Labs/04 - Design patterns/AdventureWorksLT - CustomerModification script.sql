973d064ae1e8e3c2e1f78d4d45d45a3882fe7fc2
Jason Horner <jasonh@attunix.com>
2019-02-24 20:52:48 +0100

    Update AdventureWorksLT - CustomerModification script.sql

--- Labs/04 - Design patterns/AdventureWorksLT - CustomerModification script.sql
+++ Labs/04 - Design patterns/AdventureWorksLT - CustomerModification script.sql
@@ -3,16 +3,17 @@
 USE [AdventureWorksLT]
 GO
 
+INSERT INTO [SalesLT].[Customer] 
+SELECT [NameStyle], [Title], [FirstName], [MiddleName], [LastName], [Suffix], [CompanyName], [SalesPerson], [EmailAddress], [Phone], [PasswordHash], [PasswordSalt], NEWID(), [ModifiedDate]
+FROM [SalesLT].[Customer] 
+WHERE CustomerId < 10
+GO
+
 UPDATE [SalesLT].[Customer]
    SET 
       [CompanyName] = [CompanyName] + ':UPD'
-  
  WHERE CustomerId < 10
 GO
 
 
-INSERT INTO [SalesLT].[Customer] 
 
-SELECT [NameStyle], [Title], [FirstName], [MiddleName], [LastName], [Suffix], [CompanyName], [SalesPerson], [EmailAddress], [Phone], [PasswordHash], [PasswordSalt], NEWID(), [ModifiedDate]
-FROM [SalesLT].[Customer] 
-WHERE CustomerId < 10
