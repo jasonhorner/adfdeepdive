a5580219bfa15f1c82d160a3f37b1c0b3278cb17
Jason Horner <jasonh@attunix.com>
2019-02-25 21:37:56 +0100

    foo

---
+++ Labs/00 - Prerequisites/Snippets/Master - Reset Password.sql
@@ -0,0 +1,8 @@
+ALTER LOGIN [DataLoadUser] WITH PASSWORD=N'VeryStr0ngPassw0rd!' 
+GO
+
+ALTER LOGIN [AnalysisServiceUser] WITH PASSWORD=N'VeryStr0ngPassw0rd!' 
+GO
+
+ALTER LOGIN [DeploymentUser] WITH PASSWORD=N'VeryStr0ngPassw0rd!' 
+GO
