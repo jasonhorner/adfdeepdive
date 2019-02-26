98560ea04f40f08db2c414ac1becdbc419f7e78d
Cathrine Wilhelmsen <cathrine.wilhelmsen@gmail.com>
2019-02-26 14:03:17 +0100

    Added Master Security script

---
+++ Labs/01 - ADF Overview/Azure SQL Server - Master DB Security.sql
@@ -0,0 +1,13 @@
+USE [master];
+GO
+
+CREATE USER [ADF_Access] FROM EXTERNAL PROVIDER
+
+-- Azure SQL Reader Account
+CREATE LOGIN AnalysisServiceUser WITH PASSWORD = 'ADFD3m0!';
+
+-- Azure SQL Loader Account
+CREATE LOGIN DataLoadUser WITH PASSWORD = 'ADFD3m0!';
+
+-- Azure SQL Deployment Account
+CREATE LOGIN DeploymentUser WITH PASSWORD = 'ADFD3m0!';
